ebaefa77d283253f529fbececb743b113c89cdac
David Todd Bagley <davidtoddbagley@gmail.com>
2023-03-12 23:33:25 -0500

    fix tabs

--- src/test/java/com/chat/ChatServiceTest.java
+++ src/test/java/com/chat/ChatServiceTest.java
@@ -25,21 +25,21 @@ public class ChatServiceTest {
 	UUID uuidRandom2 = UUID.randomUUID();
 
 	@Test
-    void givenEmptyChats_whenGetChatIndexByUuid_thenReturnsNull() throws InterruptedException {
+	void givenEmptyChats_whenGetChatIndexByUuid_thenReturnsNull() throws InterruptedException {
 		assertThat(chatService._getChatIndexByUuid(uuidRandom1), isNull());
-    }
+	}
 
 	@Test
-    void whenGenerateType1UUID_thenReturnsNotNullValue() throws InterruptedException {
+	void whenGenerateType1UUID_thenReturnsNotNullValue() throws InterruptedException {
 		assertThat(ChatServiceImpl._generateType1UUID(), isNotNull());
-    }
+	}
 
 	@Test
-    void givenChatUuid_whenGetChatIndexByUuid_thenReturnsTargetChat() throws InterruptedException {
+	void givenChatUuid_whenGetChatIndexByUuid_thenReturnsTargetChat() throws InterruptedException {
 		Chat chat = new Chat();
 		chat.setName("Test Chat Channel");
 		Chat createdChat = chatService.createChat(chat);
 		assertEquals(chatService.getChat(createdChat.getUuid()), createdChat);
-    }
+	}
 
 }
